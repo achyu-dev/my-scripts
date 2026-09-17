0bec45b68bc36df012141ca6a2d034bf9b50e7c0
achyu-dev <achyugeorge7744@gmail.com>
2026-09-17 23:38:33 +0530

    fix: added few roles

--- Linkedin_Job_Filter/job_filter.js
+++ Linkedin_Job_Filter/job_filter.js
@@ -324,6 +324,7 @@
         /(?:^|[^a-z0-9])BIW Design Engineer(?:$|[^a-z0-9])/i,
         /(?:^|[^a-z0-9])Payroll(?:$|[^a-z0-9])/i,
         /(?:^|[^a-z0-9])IT Engineer(?:$|[^a-z0-9])/i,
+        /(?:^|[^a-z0-9])Snowflake(?:$|[^a-z0-9])/i,
         /(?:^|[^a-z0-9])Spacecraft Controller(?:$|[^a-z0-9])/i,
         /(?:^|[^a-z0-9])Thermal Engineer(?:$|[^a-z0-9])/i,
         /(?:^|[^a-z0-9])oracle(?:$|[^a-z0-9])/i,
